c2d63f157550f166389050b9f0c2b8eff622922e
bashir uddin <uddinbashir1999@gmail.com>
2021-05-13 00:11:12 +0500

    setup api call to youtube

---
+++ src/api/youtube.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const KEY = "AIzaSyCUPLGbAoEdDuv57mtocxS1wP0LsnfjzYw";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
